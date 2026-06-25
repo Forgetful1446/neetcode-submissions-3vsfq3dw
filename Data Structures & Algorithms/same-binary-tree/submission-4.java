9ab3ab86f185edd71328426f01228c8298a67d57
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-06-25 18:19:21 +0700

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.offer(p);
+        queue.offer(q);
+
+        while(!queue.isEmpty()){
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null) return false;
+
+            if(t1.val != t2.val) return false;
+
+            queue.offer(t1.left);
+            queue.offer(t2.left);
+            queue.offer(t1.right);
+            queue.offer(t2.right);
+        }
+        return true;
+    }
+}
