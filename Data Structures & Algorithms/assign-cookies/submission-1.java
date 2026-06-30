2245744dcba90771f56531f7ade43b900aa5498b
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-06-30 16:33:49 +0700

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while (i < s.length && j < g.length){
+            if(s[i] < g[j]){
+                i++;
+            } else {
+                i++;
+                j++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
