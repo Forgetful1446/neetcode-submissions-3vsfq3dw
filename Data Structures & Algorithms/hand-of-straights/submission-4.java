158f2504bfd047083aaeb4805d89ab62b7df1fb7
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-08-02 09:26:07 +0700

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0) return false;
+
+        final int MAX_VAL = 1000;
+        int[] count = new int[MAX_VAL + 1];   // index = giá trị lá bài trực tiếp
+        for (int x : hand) count[x]++;
+
+        for (int start = 0; start <= MAX_VAL; start++) {
+            if (count[start] == 0) continue;
+
+            int need = count[start];          // số nhóm cần bắt đầu từ đây
+            for (int i = start; i < start + groupSize; i++) {
+                if (i > MAX_VAL || count[i] < need) return false;
+                count[i] -= need;
+            }
+        }
+        return true;
+    }
+}
