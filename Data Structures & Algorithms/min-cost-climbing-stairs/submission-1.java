093d4a0713b87cc5264c8372bb44060ed11a26f4
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-08-26 22:32:26 +0700

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int cost1 = cost[0];
+        int cost2 = cost[1];
+
+        for(int i = 2; i < cost.length; ++i){
+            int temp = cost[i] + Math.min(cost1, cost2);
+            cost1 = cost2;
+            cost2 = temp;
+        }
+
+        return Math.min(cost1, cost2);
+    }
+}
